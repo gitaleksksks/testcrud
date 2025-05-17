697b68b9b9783064506149ff65646450d5cfd6ec
Aleksandr <77121018+gitaleksksks@users.noreply.github.com>
2025-05-17 17:55:07 +0300

    Create TestcrudApplication.java

---
+++ src/main/java/com/example/testcrud/TestcrudApplication.java
@@ -0,0 +1,12 @@
+package com.example.testcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestcrudApplication.class, args);
+    }
+}
